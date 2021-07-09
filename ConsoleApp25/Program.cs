edc13e18a0865551fa3178dc86ffd73f945c3732
Tim Heuer <tim@timheuer.com>
2021-07-08 17:37:25 -0700

    Adding another

--- ConsoleApp25/Program.cs
+++ ConsoleApp25/Program.cs
@@ -7,6 +7,7 @@ namespace ConsoleApp25
         static void Main(string[] args)
         {
             Console.WriteLine(Resources.SomeString);
+            Console.WriteLine(Resources.AnotherString);
         }
     }
 }
